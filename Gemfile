66b306a8075357b386fbfeab4bf7f22c68ec3b6e
Alex Smith <smithavt14@gmail.com>
2018-07-24 16:00:01 +0800

    Created the basic structure

--- Gemfile
+++ Gemfile
@@ -27,6 +27,9 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+# Make fake data
+gem 'faker'
+
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,51 @@
+class ArticlesController < ApplicationController
+
+  def home
+  end
+
+  def index         # GET /restaurants
+    @articles = Article.all
+  end
+
+  def show          # GET /restaurants/:id
+    @article = Article.find(params[:id])
+  end
+
+  def new           # GET /restaurants/new
+    @article = Article.new
+  end
+
+  def create        # POST /restaurants
+    @article = Article.create(article_params)
+    @article.save
+    redirect_to articles_path
+  end
+
+  def edit          # GET /restaurants/:id/edit
+    @article = Article.find(params[:id])
+  end
+
+  def update        # PATCH /restaurants/:id
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to article_path
+  end
+
+  def destroy       # DELETE /restaurants/:id
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:article).permit(:title, :content)
+  end
+end
